4105e366563e83ff145c235f7518cbd197b606f0
Nathan Ollerenshaw <chrome@stupendous.net>
2022-03-16 23:00:27 -0700

    working filtering

--- api/api.go
+++ api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"encoding/json"
 	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/gommon/log"
 	"github.com/matjam/grupal/model"
 )
 
@@ -10,9 +12,10 @@ func Start() {
 	apiRoot := app.Group("/api")
 
 	v1 := apiRoot.Group("/v1")
-	v1.Get("/user/:id", GetHandler[model.User]("id"))
-	v1.Get("/user/email/:email", GetHandler[model.User]("email"))
-	v1.Post("/user", PostHandler[model.User]())
+	v1.Get("/users/:id", GetHandler[model.User]("id"))
+	v1.Get("/users", SearchHandler[model.User]())
+	v1.Post("/users", PostHandler[model.User]())
+	v1.Put("/users/:id", PutHandler[model.User]("id"))
 	app.Listen(":3000")
 }
 
@@ -42,5 +45,54 @@ func PostHandler[T any]() fiber.Handler {
 			return c.JSON(err)
 		}
 		return c.JSON(newRow)
+	}
+}
+
+func PutHandler[T any](field string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		row := new(T)
+		err := c.BodyParser(row)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func SearchHandler[T any]() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ft := map[string]any{}
+		ftString := c.Query("filter", "{}")
+		err := json.Unmarshal([]byte(ftString), &ft)
+		if err != nil {
+			log.Errorf("bad filter %v", ftString)
+			return c.Status(fiber.StatusBadRequest).SendString("Unacceptable filter")
+		}
+
+		var st []string
+		stString := c.Query("sort", "[]")
+		err = json.Unmarshal([]byte(stString), &st)
+		if err != nil || (len(st) != 2 && len(st) != 0) {
+			log.Errorf("bad sort %v", stString)
+			return c.Status(fiber.StatusBadRequest).SendString("Unacceptable sort")
+		}
+
+		var rg []int
+		rgString := c.Query("range", "[0,0]")
+		err = json.Unmarshal([]byte(rgString), &rg)
+		if err != nil || (len(rg) != 2 && len(rg) != 0) {
+			log.Errorf("bad range %v", rgString)
+			return c.Status(fiber.StatusBadRequest).SendString("Unacceptable range")
+		}
+
+		log.Infof("GET %v filter %v sort %v range %v", c.Request().URI().String(), ft, st, rg)
+
+		result, err := model.Search[T](ft, rg[0], rg[1], st)
+		if err != nil {
+			log.Errorf("database error: %v", err.Error())
+			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+		}
+		return c.JSON(result)
 	}
 }
--- model/model.go
+++ model/model.go
@@ -39,10 +39,33 @@ func Create[T any](row T) (*T, error) {
 }
 
 func Update[T any](row T) (*T, error) {
-	tt := new(T) // just used to tell gorm what model we're using, kind of annying actually
+	tt := new(T) // just used to tell gorm what model we're using, hopefully can go away with generics
 	err := DB.Model(tt).Updates(row).Error
 	if err != nil {
 		return nil, err
 	}
 	return &row, nil
+}
+
+func Search[T any](filter map[string]any, limit int, offset int, order []string) ([]T, error) {
+	var results []T
+
+	// may not work for where IN [list of things]
+	q := DB.Where(filter)
+
+	if len(order) == 2 {
+		q = q.Order(order[0] + " " + order[1])
+	}
+
+	// not sure how a limit of 0 could ever be what you want.
+	if limit > 0 {
+		q = q.Limit(limit)
+	}
+
+	if offset > 0 {
+		q = q.Offset(offset)
+	}
+
+	err := q.Find(&results).Error
+	return results, err
 }
